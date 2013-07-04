d298e0517f35057ec4d841f0a1b108cdb6583eff
Tyler Lab Linux <Tyler Lab Linux@localhost>
2013-07-04 16:30:03 +0900

    Started adding separate feature finder class

--- Rover/cpp/FeatureFinder.h
+++ Rover/cpp/FeatureFinder.h
@@ -1,22 +1,21 @@
-#ifndef ICSL_VISIONPROCESSOR_H
-#define ICSL_VISIONPROCESSOR_H
+#ifndef ICSL_VISIOFINDER_H
+#define ICSL_VISIONFINDER_H
 #include <memory>
 #include <sched.h>
 #include <cmath>
 #include <list>
 
 #include <toadlet/egg.h>
-//using toadlet::egg::String;
 
 #include <opencv2/core/core.hpp>
-#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/features2d/features2d.hpp>
-#include <opencv2/video/tracking.hpp>
-#include <opencv2/objdetect/objdetect.hpp>
+//#include <opencv2/features2d/features2d.hpp>
+//#include <opencv2/video/tracking.hpp>
+//#include <opencv2/objdetect/objdetect.hpp>
 
-#include "TNT/tnt.h"
-#include "TNT_Utils.h"
+//#include "TNT/tnt.h"
+//#include "TNT_Utils.h"
 
 #include "constants.h"
 #include "QuadLogger.h"
@@ -25,30 +24,25 @@
 #include "Time.h"
 #include "CommManager.h"
 #include "SensorManager.h"
-#include "BlobDetector.h"
 #include "Data.h"
-
-#include "Matcher.h"
 
 namespace ICSL {
 namespace Quadrotor {
-class VisionProcessorListener
+class FeatureFinderListener
 {
 	public:
-		virtual ~VisionProcessorListener(){};
+		virtual ~FeatureFinderListener(){};
 
-		virtual void onImageProcessed(shared_ptr<ImageMatchData> const &data)=0;
-		virtual void onImageTargetFound(shared_ptr<ImageTargetFindData> const data)=0;
-		virtual void onImageLost()=0;
+		virtual void onFeaturesFound(shared_ptr<ImageFeatureData> const data)=0;
 };
 
-class VisionProcessor : public toadlet::egg::Thread, 
+class FeatureFinder : public toadlet::egg::Thread, 
 						public CommManagerListener,
 						public SensorManagerListener
 {
 	public:
-		explicit VisionProcessor();
-		virtual ~VisionProcessor(){};
+		explicit FeatureFinder();
+		virtual ~FeatureFinder(){};
 
 		void shutdown();
 		void initialize();
@@ -68,13 +62,11 @@ class VisionProcessor : public toadlet::egg::Thread,
 		void getLastImageAnnotated(cv::Mat *outImage);
 		toadlet::egg::Collection<int> getVisionParams();
 
-		void addListener(VisionProcessorListener *listener){mListeners.push_back(listener);}
+		void addListener(FeatureFinderListener *listener){mListeners.push_back(listener);}
 
 		// CommManagerListener functions
 		void onNewCommLogMask(uint32 mask);
 		void onNewCommImgBufferSize(int size);
-		void onNewCommVisionRatioThreshold(float h);
-		void onNewCommVisionMatchRadius(float r);
 		void onCommConnectionLost();
 		void onNewCommMotorOn(){mMotorOn = true;}
 		void onNewCommMotorOff(){mMotorOn = false;}
@@ -87,52 +79,37 @@ class VisionProcessor : public toadlet::egg::Thread,
 		bool mUseIbvs;
 		bool mFirstImageProcessed;
 		bool mRunning, mFinished;
-		bool mNewImageReady_featureMatch, mNewImageReady_targetFind;
+		bool mNewImageReady, mNewImageReady_targetFind;
 		bool mLogImages;
 		cv::Mat	mCurImage, mCurImageGray;
 		shared_ptr<cv::Mat> mCurImageAnnotated;
 		vector<vector<double> > mMSERHuMoments;
-		vector<cv::Point2f> mMSERCentroids;
 
 		shared_ptr<DataImage> mImageDataPrev, mImageDataCur, mImageDataNext;
 
-		Time mStartTime, mLastImgFoundTime, mLastProcessTime;
+		Time mStartTime, mLastProcessTime;
 
 		toadlet::uint32 mImgProcTimeUS;
 
 		QuadLogger *mQuadLogger;
 
-		toadlet::egg::Mutex mMutex_data, mMutex_image, mMutex_imageData, mMutex_buffers, mMutex_matcher;
+		toadlet::egg::Mutex mMutex_data, mMutex_image, mMutex_imageData, mMutex_buffers;
 		toadlet::egg::Mutex mMutex_logger;
 
-		Collection<VisionProcessorListener*> mListeners;
+		Collection<FeatureFinderListener*> mListeners;
 
 		void run();
 
 		int mImgBufferMaxSize;
-		list<shared_ptr<DataImage> > mImgDataBuffer;
-		list<shared_ptr<ImageMatchData> > mImgMatchDataBuffer;
-		list<vector<vector<cv::Point2f> > > mFeatureMatchBuffer;
-		list<Time> mFeatureMatchTimeBuffer;
-		list<double> mFeatureMatchDTBuffer;
-		list<TNT::Array2D<double> > mFeatureMatchAttPrevBuffer, mFeatureMatchAttCurBuffer;
 
-		Matcher mFeatureMatcher;
-
-		vector<vector<cv::Point2f> > getMatchingPoints(cv::Mat const &img);
-		vector<BlobDetector::Blob> findCircles(cv::Mat const &img);
-		static void drawMatches(vector<vector<cv::Point2f> > const &points, cv::Mat &img);
-		static void drawTarget(vector<BlobDetector::Blob> const &circles, cv::Mat &img);
-
-		void runTargetFinder();
+		vector<cv::Point2f> findFeaturePoints(cv::Mat const &img);
+		static void drawPoints(vector<cv::Point2f> const &points, cv::Mat &img);
 
 		bool mMotorOn;
 		int mThreadPriority, mScheduler;
-
-		vector<BlobDetector::Blob> mLastCircles;
 };
 
 } // namespace Quadrotor
 } // namespace ICSL
 
-#endif // VISIONPROCESSOR_H
+#endif
