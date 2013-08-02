7e57ea676f247131976bdf6c1a34a36c1e37590b
Tyler Lab Linux <Tyler Lab Linux@localhost>
2013-08-03 08:12:12 +0900

    Working on new comm with arduino

---
+++ arduino/onboard/MotorInterfaceS4/AndroidTestApp/src/icsl/serialtest/SerialConsoleActivity.java
@@ -0,0 +1,214 @@
+/* Copyright 2011 Google Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301,
+ * USA.
+ *
+ * Project home page: http://code.google.com/p/usb-serial-for-android/
+ */
+
+package com.icsl.serialtest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.util.HexDump;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.nio.ByteBuffer;
+
+/**
+ * Monitors a single {@link UsbSerialDriver} instance, showing all data
+ * received.
+ *
+ * @author mike wakerly (opensource@hoho.com)
+ */
+public class SerialConsoleActivity extends Activity {
+
+    private final String TAG = SerialConsoleActivity.class.getSimpleName();
+
+    /**
+     * Driver instance, passed in statically via
+     * {@link #show(Context, UsbSerialDriver)}.
+     *
+     * <p/>
+     * This is a devious hack; it'd be cleaner to re-create the driver using
+     * arguments passed in with the {@link #startActivity(Intent)} intent. We
+     * can get away with it because both activities will run in the same
+     * process, and this is a simple demo.
+     */
+    private static UsbSerialDriver sDriver = null;
+
+    private TextView mTitleTextView;
+    private TextView mDumpTextView;
+    private ScrollView mScrollView;
+
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+
+    private SerialInputOutputManager mSerialIoManager;
+
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+
+        @Override
+        public void onRunError(Exception e) {
+            Log.d(TAG, "Runner stopped.");
+        }
+
+		private final static byte HEX_NEWLINE_STR=0x0A;
+		private final static byte HEX_CARRIAGE_RETURN_STR=0x0D;
+		private ByteBuffer msgBuffer = ByteBuffer.allocate(128);
+        @Override
+        public void onNewData(final byte[] data) {
+            SerialConsoleActivity.this.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+					msgBuffer.put(data);
+					if(data[data.length-1] == 0x0A)
+					{
+//						mDumpTextView.append(new String(msgBuffer.array()));
+						float val1 = Float.parseFloat(new String(msgBuffer.array()));
+						int val = (int)val1;
+//						int val = Integer.parseInt(new String(msgBuffer.array()));
+						mDumpTextView.append( String.valueOf(val) + "\n");
+						msgBuffer = ByteBuffer.allocate(128);
+
+//						int valBytes = Float.floatToIntBits(val);
+						int valBytes = val;
+						byte[] chad = new byte[4];
+						chad[3] = (byte)(valBytes >> 3*8);
+						chad[2] = (byte)(valBytes >> 2*8);
+						chad[1] = (byte)(valBytes >> 1*8);
+						chad[0] = (byte)(valBytes >> 0*8);
+Log.i(TAG,"Bob is: " + Integer.toBinaryString(valBytes));
+
+//						chad[4] = (byte)0xFF; // terminate character
+
+						byte[] bob = new byte[1];
+						bob[0] = 3;
+						int timeoutMS = 500;
+						try
+						{
+							sDriver.write(chad, timeoutMS);
+						}
+						catch(Exception ex){};
+					}
+
+					mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
+                }
+            });
+        }
+    };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.serial_console);
+        mTitleTextView = (TextView) findViewById(R.id.demoTitle);
+        mDumpTextView = (TextView) findViewById(R.id.consoleText);
+        mScrollView = (ScrollView) findViewById(R.id.demoScroller);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopIoManager();
+        if (sDriver != null) {
+            try {
+                sDriver.close();
+            } catch (IOException e) {
+                // Ignore.
+            }
+            sDriver = null;
+        }
+        finish();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Resumed, sDriver=" + sDriver);
+        if (sDriver == null) {
+            mTitleTextView.setText("No serial device.");
+        } else {
+            try {
+                sDriver.open();
+                sDriver.setParameters(115200, 8, UsbSerialDriver.STOPBITS_1, UsbSerialDriver.PARITY_NONE);
+            } catch (IOException e) {
+                Log.e(TAG, "Error setting up device: " + e.getMessage(), e);
+                mTitleTextView.setText("Error opening device: " + e.getMessage());
+                try {
+                    sDriver.close();
+                } catch (IOException e2) {
+                    // Ignore.
+                }
+                sDriver = null;
+                return;
+            }
+            mTitleTextView.setText("Serial device: " + sDriver.getClass().getSimpleName());
+        }
+        onDeviceStateChange();
+    }
+
+    private void stopIoManager() {
+        if (mSerialIoManager != null) {
+            Log.i(TAG, "Stopping io manager ..");
+            mSerialIoManager.stop();
+            mSerialIoManager = null;
+        }
+    }
+
+    private void startIoManager() {
+        if (sDriver != null) {
+            Log.i(TAG, "Starting io manager ..");
+            mSerialIoManager = new SerialInputOutputManager(sDriver, mListener);
+            mExecutor.submit(mSerialIoManager);
+        }
+    }
+
+    private void onDeviceStateChange() {
+        stopIoManager();
+        startIoManager();
+    }
+
+//    private void updateReceivedData(byte[] data) {
+//        final String message = "Read " + data.length + " bytes: \n"
+//                + HexDump.dumpHexString(data) + "\n\n";
+//        mDumpTextView.append(message);
+//        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
+//    }
+
+    /**
+     * Starts the activity, using the supplied driver instance.
+     *
+     * @param context
+     * @param driver
+     */
+    static void show(Context context, UsbSerialDriver driver) {
+        sDriver = driver;
+        final Intent intent = new Intent(context, SerialConsoleActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_NO_HISTORY);
+        context.startActivity(intent);
+    }
+
+}
---
+++ arduino/onboard/MotorInterfaceS4/MotorInterfaceS4.ino
@@ -0,0 +1,56 @@
+
+union
+{
+  char byteVal[4];
+  float floatVal;
+  int intVal;
+} serialFloat;
+
+void setup()
+{
+  delay(500);
+  Serial.begin(115200);
+  pinMode(13,OUTPUT);
+  
+  serialFloat.intVal = 0;
+  while(Serial.available())
+    Serial.read();
+}
+
+//float val = -1.325;
+int val = 0;
+int timeoutMS = 500;
+void loop()
+{
+  Serial.println(val);
+  
+  while(Serial.available() == 0)
+    delay(10);
+    
+  if(Serial.available() > 0 )
+  {
+    int start = millis();
+    while(Serial.available() < 4 && millis()-start < timeoutMS)
+      delay(5);
+    
+    if(Serial.available() >= 4)
+    {
+//      Serial.readBytes(serialFloat.byteVal, 4);
+//      Serial.read();
+//      serialFloat.intVal = Serial.parseInt();
+//      Serial.readBytesUntil(0xFF, serialFloat.byteVal, 10);
+      for(int i=0; i<4; i++)
+        serialFloat.byteVal[i] = Serial.read();
+      
+//      val = serialFloat.floatVal+0.25;
+      val = serialFloat.intVal+1;
+//      val = Serial.parseInt()+1;
+//      val = Serial.read()+1;
+    }
+    
+    while(Serial.available() > 0)
+      Serial.read();
+  }
+  
+  delay(500);
+}
