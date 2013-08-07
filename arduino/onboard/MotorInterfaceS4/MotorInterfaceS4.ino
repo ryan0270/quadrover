56fca88ae0f7490c0b7726b094610828311d27c2
lappy <lappy@localhost>
2013-08-07 12:32:32 +0900

    temp

--- arduino/onboard/MotorInterfaceS4/MotorInterfaceS4.ino
+++ arduino/onboard/MotorInterfaceS4/MotorInterfaceS4.ino
@@ -4,18 +4,26 @@ union
   char byteVal[4];
   float floatVal;
   int intVal;
-} serialFloat;
+} 
+serialFloat;
 
+#define LED_PIN 12
+unsigned long lastCommTime;
 void setup()
 {
   delay(500);
   Serial.begin(115200);
-  pinMode(13,OUTPUT);
-  
+  pinMode(LED_PIN,OUTPUT);
+  digitalWrite(LED_PIN,LOW);
+
   serialFloat.intVal = 0;
   while(Serial.available())
     Serial.read();
+
+  lastCommTime = 0;
 }
+
+boolean isConnected = false;
 
 float val = -1.325;
 //int val = 0;
@@ -23,34 +31,37 @@ int timeoutMS = 500;
 void loop()
 {
   Serial.println(val,4);
-  
+
   while(Serial.available() == 0)
+  {
+    if(millis()-lastCommTime > 500)
+      isConnected = false;
+    if(!isConnected)
+      digitalWrite(LED_PIN,LOW);    
     delay(10);
-    
+  }
+
   if(Serial.available() > 0 )
   {
     int start = millis();
     while(Serial.available() < 4 && millis()-start < timeoutMS)
       delay(5);
-    
+
     if(Serial.available() >= 4)
     {
-//      Serial.readBytes(serialFloat.byteVal, 4);
-//      Serial.read();
-//      serialFloat.intVal = Serial.parseInt();
-//      Serial.readBytesUntil(0xFF, serialFloat.byteVal, 10);
+      lastCommTime = millis();
+      isConnected = true;
+      digitalWrite(LED_PIN,HIGH);
       for(int i=0; i<4; i++)
         serialFloat.byteVal[i] = Serial.read();
-      
+
       val = serialFloat.floatVal+0.25;
-//      val = serialFloat.intVal+1;
-//      val = Serial.parseInt()+1;
-//      val = Serial.read()+1;
     }
-    
+
     while(Serial.available() > 0)
       Serial.read();
   }
-  
-  delay(500);
+
+  delay(400);
 }
+
