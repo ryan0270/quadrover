3d62b745bfc4daa11dfb9ff885f8914f5cee41ef
Tyler Lab Linux <Tyler Lab Linux@localhost>
2013-08-09 15:14:43 +0900

    misc

--- arduino/onboard/MotorInterfaceS4/MotorInterfaceS4.ino
+++ arduino/onboard/MotorInterfaceS4/MotorInterfaceS4.ino
@@ -15,6 +15,11 @@ void setup()
   Serial.begin(115200);
   pinMode(LED_PIN,OUTPUT);
   digitalWrite(LED_PIN,LOW);
+  
+  pinMode(9,OUTPUT);
+  digitalWrite(9,LOW);
+  pinMode(8,OUTPUT);
+  digitalWrite(8,HIGH);
 
   serialFloat.intVal = 0;
   while(Serial.available())
@@ -25,22 +30,22 @@ void setup()
 
 boolean isConnected = false;
 
-float val = -1.325;
+float val = 127/255;
 //int val = 0;
 int timeoutMS = 500;
 void loop()
 {
+  val += 0.01;
+  if(isConnected)
+    analogWrite(LED_PIN, (int)(val*255));
+  else
+    digitalWrite(LED_PIN, LOW);
+  
   Serial.println(val,4);
 
-  while(Serial.available() == 0)
-  {
-    if(millis()-lastCommTime > 500)
-      isConnected = false;
-    if(!isConnected)
-      digitalWrite(LED_PIN,LOW);    
-    delay(10);
-  }
-
+  if(millis()-lastCommTime > 500)
+    isConnected = false;
+  
   if(Serial.available() > 0 )
   {
     int start = millis();
@@ -51,17 +56,17 @@ void loop()
     {
       lastCommTime = millis();
       isConnected = true;
-      digitalWrite(LED_PIN,HIGH);
+//      digitalWrite(LED_PIN,HIGH);
       for(int i=0; i<4; i++)
         serialFloat.byteVal[i] = Serial.read();
-
-      val = serialFloat.floatVal+0.25;
+        
+      val = serialFloat.floatVal;
     }
 
     while(Serial.available() > 0)
       Serial.read();
   }
 
-  delay(400);
+  delay(100);
 }
 
