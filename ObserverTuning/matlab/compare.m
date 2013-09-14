7f2ef892d841ae0f657117d5c51306e650903126
Tyler Lab Linux <Tyler Lab Linux@localhost>
2013-09-14 18:50:07 +0900

    Retuned things so now I'm doing better than before

--- ObserverTuning/matlab/compare.m
+++ ObserverTuning/matlab/compare.m
@@ -5,6 +5,7 @@ disp('start chadding')
 log_ids
 
 %%
+% dataDir = '../dataSets/Sep8';
 dataDir = '../dataSets/Sep12';
 viconFile = [dataDir '/pcData.txt'];
 viconData = importdata(viconFile,'\t',0);
@@ -98,47 +99,47 @@ tranStateLabels = { 'x [m]' 'y [m]' 'z [m]' 'x vel [m/s]' 'y vel [m/s]' 'z vel [
 
 %%
 if exist('angleState','var') && ~isempty(angleState)
-%   	figure(0); clf;
+% %   	figure(0); clf;
+% % % 	set(gcf,'Units','Inches');
+% % % 	curPos = get(gcf,'Position'); figSize = [6 4];
+% % % 	set(gcf,'PaperSize',figSize,'PaperPosition',[0 0 figSize],'Position',[curPos(1:2) figSize]);
+% % 	mask = find( (viconStateTime > angleStateTime(1)) .* (viconStateTime <= angleStateTime(end) ) );
+% % 	for i=1:6
+% % 		subplot(2,3,i);		
+% % 		plot(viconStateTime(mask), viconState(i,mask)); hold all
+% % 		plot(angleStateTime, angleState(i,:)); hold all
+% % 		hold off
+% % 
+% % 		xlabel('Time [s]')
+% % 		ylabel(angleStateLabels(i));	
+% % 	end
+% 
+%   	figure(1); clf;
 % % 	set(gcf,'Units','Inches');
 % % 	curPos = get(gcf,'Position'); figSize = [6 4];
 % % 	set(gcf,'PaperSize',figSize,'PaperPosition',[0 0 figSize],'Position',[curPos(1:2) figSize]);
 % 	mask = find( (viconStateTime > angleStateTime(1)) .* (viconStateTime <= angleStateTime(end) ) );
+% 	timeShift = 0.1;
 % 	for i=1:6
-% 		subplot(2,3,i);		
-% 		plot(viconStateTime(mask), viconState(i,mask)); hold all
-% 		plot(angleStateTime, angleState(i,:)); hold all
+% 		subplot(2,3,i);
+% 		plot(viconStateTime(mask)-timeShift, viconState(i,mask)-mean(viconState(i,mask))); hold all
+% 		plot(angleStateTime, angleState(i,:)-mean(angleState(i,:))); hold all
 % 		hold off
 % 
 % 		xlabel('Time [s]')
-% 		ylabel(angleStateLabels(i));	
+% 		ylabel(angleStateLabels(i));
 % 	end
-
-  	figure(1); clf;
-% 	set(gcf,'Units','Inches');
-% 	curPos = get(gcf,'Position'); figSize = [6 4];
-% 	set(gcf,'PaperSize',figSize,'PaperPosition',[0 0 figSize],'Position',[curPos(1:2) figSize]);
-	mask = find( (viconStateTime > angleStateTime(1)) .* (viconStateTime <= angleStateTime(end) ) );
-	timeShift = 0.1;
-	for i=1:6
-		subplot(2,3,i);
-		plot(viconStateTime(mask)-timeShift, viconState(i,mask)-mean(viconState(i,mask))); hold all
-		plot(angleStateTime, angleState(i,:)-mean(angleState(i,:))); hold all
-		hold off
-
-		xlabel('Time [s]')
-		ylabel(angleStateLabels(i));
-	end
-	
-	viconStateAngleInterp = interp1(viconStateTime, viconState', angleStateTime+timeShift,[],'extrap')';
-	start = max([find(abs(angleStateTime(1,:)) > 0.05,1,'first') find(angleStateTime > mapVelTime(1),1,'first')]);
-	err = viconStateAngleInterp(1:3,start:end)-angleState(1:3,start:end);
-	err = err-diag(mean(err,2))*ones(size(err));
-	rmsErr = rms(err')';
-	fprintf('Angle state rms err:\t');
-	for i=1:3
-		fprintf('%1.3f\t',rmsErr(i));
-	end
-	fprintf('\n');
+% 	
+% 	viconStateAngleInterp = interp1(viconStateTime, viconState', angleStateTime+timeShift,[],'extrap')';
+% 	start = max([find(abs(angleStateTime(1,:)) > 0.05,1,'first') find(angleStateTime > mapVelTime(1),1,'first')]);
+% 	err = viconStateAngleInterp(1:3,start:end)-angleState(1:3,start:end);
+% 	err = err-diag(mean(err,2))*ones(size(err));
+% 	rmsErr = rms(err')';
+% 	fprintf('Angle state rms err:\t');
+% 	for i=1:3
+% 		fprintf('%1.3f\t',rmsErr(i));
+% 	end
+% 	fprintf('\n');
 end
 
 %%
